3afb850887f8ef0c1a2ab28fb6863fa9c3e73c63
gari30 <gariyoshi0630@gmail.com>
2021-05-08 23:56:43 +0900

    割り込みの追加とボタン押したときの動作を追加

--- sw_light-on.cpp
+++ sw_light-on.cpp
@@ -1,19 +1,66 @@
+#include <cstdint>
+#include <functional>
 #include <stdio.h>
+#include <stdlib.h>
 
-#include "sw_light-on.h"
 #include "pigpiod_if2.h"
 
+#include "sw_light-on.h"
+
+#define TACTSW_PIN 7
+
+void GpioEventNotify(int, unsigned int, unsigned int, unsigned int);
+void TactSwPushEvent(uint8_t level);
+
 GpioPi::GpioPi() {
-  pi = pigpio_start(NULL, NULL);
+  pi_ = pigpio_start(NULL, NULL);
+  TackSwitchPinInit();
 }
 
 GpioPi::~GpioPi() {
-  pigpio_stop(pi);
+  pigpio_stop(pi_);
+}
+
+void GpioPi::TackSwitchPinInit() {
+  set_mode(pi_, TACTSW_PIN, PI_INPUT); // GPIOをinputに設定
+  set_pull_up_down(pi_, TACTSW_PIN, PI_PUD_UP); // GPIOをプルアップに設定
+  set_noise_filter(pi_, TACTSW_PIN, kSamplingTime, 1000/*kSamplingTime*/); // チャタキャンセル
+}
+
+void GpioPi::SetCallback() {
+  callback(pi_, TACTSW_PIN, FALLING_EDGE, &GpioEventNotify); // コールバックの登録
+}
+
+/* param
+ *  pi    : pigpio no.(no use)
+ *  gpio  : 0-31
+ *  level : 0:change to low
+ *          1:change to high
+ *          2:no level change(a watchdog timeout)
+ *  tick  : timestamp
+ */
+void GpioEventNotify(int pi, unsigned int gpio, unsigned int level, unsigned int tick) {
+  switch(gpio) {
+    case TACTSW_PIN:
+      TactSwPushEvent(static_cast<uint8_t>(level));
+      break;
+    
+    default:
+      break;
+  }
+}
+
+void TactSwPushEvent(uint8_t level) {
+//  printf("TactSwitch push. level:%d\n", level);
+  system("/home/garipi/homebot/bin/pi-send.sh 'room2lighton'");
 }
 
 int main(void) {
+  GpioPi gpiopi;
 
-  printf("test\n");
+  gpiopi.SetCallback();
+
+  while(1){};
 
   return 0;
 }
--- sw_light-on.h
+++ sw_light-on.h
@@ -1,13 +1,22 @@
+#pragma once
 
 class GpioPi {
  public:
-
- private:
   GpioPi();
   ~GpioPi();
-  
-//  void TackSwitchPinInit(); //タクトスイッチPinの初期化
+  void SetCallback(); // GPIOの入力値変化通知の設定
 
-  int pi;
+ private:
+  void TackSwitchPinInit(); // タクトスイッチPinの初期化
+//  void GpioEventNotify(int, unsigned int, unsigned int, unsigned int); // コールバック関数
+
+  int32_t pi_; // pigpioをstart結果
+
+  std::function<void(uint8_t)> tactsw_notify_;
+  // ↑今後監視するポートが増えたら配列にする
+  //std::array<std::function, kTactSwPin> notify_func_list;
+
+  static constexpr auto kSamplingTime = 200*1000; // チャタキャンセルのサンプリング数(us)
+
 };
 
